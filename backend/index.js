d0cab8dcae1b24155c7b4acd28121a549775a5c1
Anupama Neupane <anupaneupane1994@gmail.com>
2025-04-06 13:25:21 +0545

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use()
+
+
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server running at port${PORT}`);
+})
